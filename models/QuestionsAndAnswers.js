5667c82cc2cd389815fcc1983373236cd2a03ee5
Andymamooo <andyderkaema@gmail.com>
2023-01-30 21:06:25 -0800

    one test working and got answer and post question query working

--- models/QuestionsAndAnswers.js
+++ models/QuestionsAndAnswers.js
@@ -34,16 +34,29 @@ const getQuestionsQuery = (product_id, limit) => {
                     'photos',(SELECT coalesce (json_agg(url), '[]'::json) FROM photo WHERE photo.answer_id = answer.id)
                   )
                 )
-              ), '{}'::json )FROM ANSWER WHERE answer.question_id = question.id
+              ), '{}'::json )FROM (SELECT * FROM ANSWER WHERE answer.question_id = question.id) AS answer
             )
           )
-         ) FROM question where product_id = ${Number(product_id)} limit ${Number(limit)}
+         ) FROM (SELECT * FROM question WHERE product_id = ${Number(product_id)} limit ${Number(limit)}) AS question
       )
     )`
   )
 };
 
-const getAnswersQuery = (question_id, limit) => {
+const postQuestionsQuery = async (newQuestion) => {
+  return (
+    pool.connect().then((client) => {
+      return client
+      .query (`INSERT INTO question(product_id, body, date_written,asker_name,asker_email,reported,helpful)
+      VALUES ( $1, $2, (SELECT extract(epoch FROM  now()) * 1000), $3, $4, false, 0) RETURNING id`, [newQuestion.id, newQuestion.body, newQuestion.name, newQuestion.email])
+      .then ((result) => {
+        return result;
+      })
+    })
+  )
+}
+const getAnswersQuery =  (question_id, limit) => {
+  console.log(`question id is equal to ${question_id}`);
   var stringifiedID = "'"+question_id+"'";
   return (
     `SELECT json_build_object (
@@ -53,48 +66,75 @@ const getAnswersQuery = (question_id, limit) => {
           json_build_object (
             'answer_id', id,
             'body', body,
-            'date', date_written,
+            'date', ( SELECT to_char(to_timestamp(date_written/1000),'YYYY-MM-DD"T"HH24:MI:SS"Z"')),
             'answerer_name', answerer_name,
             'helpfulness', helpful,
             'photos', (
-              SELECT json_agg (
+              SELECT coalesce ( json_agg (
                 json_build_object (
                   'id', id,
                   'url', url
                 )
-              ) FROM photo WHERE photo.answer_id = answer.id
+                ),'[]'::json) FROM ( SELECT * FROM photo WHERE photo.answer_id = answer.id) AS photo
             )
           )
-        ) FROM ANSWER WHERE question_id = ${question_id}
+        ) FROM (SELECT * FROM answer WHERE question_id = ${Number(question_id)} limit ${limit}) AS answer
       )
     )`
   )
 }
 
-const addQuestionQuery = (question) => {
-  console.log(`inputted question is equal to ${JSON.stringify(question) }`)
-
-}
 
 module.exports = {
 
+  query: (text,values) => {
+    return pool.query(text, values);
+  },
   getQuestions: async (product_id, limit) => {
     var query = getQuestionsQuery(product_id,limit);
-    console.log(`question_id is equal to ${product_id}`);
+    //console.log(`question_id is equal to ${product_id}`);
     return pool.connect().then((client) => {
       return client
       .query(query)
       .then((result) => {
-        console.log(`result for getQuestion is equal to ${JSON.stringify(result.rows[0].json_build_object)}`)
+        //console.log(`result for getQuestion is equal to ${JSON.stringify(result.rows[0].json_build_object)}`)
         return result.rows[0].json_build_object
+        //return result.rows
+      })
+      .catch((err) => {
+        console.log(`err in getQuestions is equal to ${err}`);
+        throw(err);
       })
     })
 
   },
 
+  getAnswers: async (question_id, limit) => {
+    var query = getAnswersQuery(question_id, limit);
+    return pool.connect().then((client) => {
+      return client
+      .query(query)
+      .then((result) => {
+        return result.rows[0].json_build_object
+      })
+      .catch((err) => {
+        console.log(`err in getAnswer is equal to ${err}`);
+        throw(err);
+      })
+    })
+  },
+
+
   addQuestion: async(question) => {
-    addQuestionQuery(question);
-    return;
+    console.log(`question is equal to ${JSON.stringify(question)}`);
+     postQuestionsQuery(question)
+     .then ((result) => {
+      console.log(`result after adding into db is equal to ${JSON.stringify(result.rows[0].id)}`);
+      return result.rows[0].id
+     })
+     .catch((err) => {
+      console.log(`err in addQuestion `)
+     })
   }
 // pool.connect().then((client) => {
 //   return client
@@ -141,5 +181,29 @@ module.exports = {
 //         )
 //       )
 //      ) FROM question where product_id = 1 limit 10
+//   )
+// )
+
+
+// SELECT json_build_object (
+//   'question' , 1,
+//   'results', (
+//     SELECT json_agg (
+//       json_build_object (
+//         'answer_id', id,
+//         'body', body,
+//         'date', ( SELECT to_char(to_timestamp(date_written/1000),'YYYY-MM-DD"T"HH24:MI:SS"Z"')),
+//         'answerer_name', answerer_name,
+//         'helpfulness', helpful,
+//         'photos', (
+//           SELECT coalesce ( json_agg (
+//             json_build_object (
+//               'id', id,
+//               'url', url
+//             )
+//           ), '{}'::json ) FROM photo WHERE photo.answer_id = answer.id
+//         )
+//       )
+//     ) FROM (SELECT * FROM answer WHERE question_id = 1 limit 1) AS answer
 //   )
 // )
--- routes/questions.js
+++ routes/questions.js
@@ -11,11 +11,37 @@ router.get('/', (req,res) => {
     console.log(JSON.stringify(result));
     res.send(result);
   })
+  .catch ((err) => {
+    console.log(`err while getting question : ${err}`)
+    throw(err)
+  })
 })
 
-router.post('/')
-router.get('/:question_id/answers', (req,res) => {
-  res.send('question answers!')
+router.post('/', (req, res) => {
+  QuestionsAndAnswers.addQuestion(req.query)
+  .then ((result) => {
+    console.log(`result inside post router is equal to ${result}`);
+    res.send(result);
+  })
+  .catch((err) => {
+    console.log(`err while posting question : ${err}`)
+    throw(err)
+  })
+})
+
+router.get('/:question_id/answers', (req, res) => {
+  var question_id = req.params.question_id;
+  var limit = req.query.count || 5;
+  QuestionsAndAnswers.getAnswers(question_id, limit)
+  .then((result) => {
+    res.send(result)
+  })
 })
 
 module.exports = router
+
+
+/*
+
+INSERT INTO question(product_id, body, date_written,asker_name,asker_email,reported,helpful) VALUES ( 1, 'testinggggg', (SELECT extract(epoch FROM  now())), 'andy', 'andy@test.com', false, 0);
+*/
